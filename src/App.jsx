28ad7dd0cd5533065c8011d5eec6726d5dada3c2
Luke Eades <lukeeadeswork@gmail.com>
2025-05-30 20:51:53 -0700

    added basic settings form

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,19 @@ const times = {
   longBreak: 0.3,
 }
 const App = () => {
+  const [settings, setSettings] = useState({
+    times,
+  })
   const [type, setType] = useState("work")
-  const [time, setTime] = useState(times[type] * 60)
+  const [time, setTime] = useState(settings.times[type] * 60)
   const [paused, setPaused] = useState(true)
   const [sessions, setSessions] = useState(0)
   useEffect(() => {
     const intervalID = setInterval(() => {
+      if (paused) return
       setTime(prev => {
-        if (paused || prev < 0) return prev
+        if (prev < 0) return prev
         if (prev === 0) {
-          // set to next type and reset timer
           timerEnd()
           return times[type] * 60
         } else if (prev > 0) return prev - 1
@@ -44,6 +47,12 @@ const App = () => {
     setType(timerType)
     setTime(times[timerType] * 60)
     setPaused(true)
+  }
+  const changeSettings = newSettings => {
+    setSettings(newSettings)
+    if (paused) {
+      setTime(newSettings.times[type] * 60)
+    }
   }
   return (
     <div>
@@ -87,6 +96,7 @@ const App = () => {
           Reset
         </button>
       </nav>
+      <Settings setSettings={changeSettings} settings={settings} />
     </div>
   )
 }
@@ -97,6 +107,54 @@ const Timer = ({ seconds }) => {
   return (
     <div>
       {padTime(Math.floor(seconds / 60))}:{padTime(seconds % 60)}
+    </div>
+  )
+}
+const Settings = ({ settings, setSettings }) => {
+  const [formSettings, setFormSettings] = useState({
+    ...settings,
+  })
+  const saveSettings = e => {
+    e.preventDefault()
+    setSettings(formSettings)
+  }
+  const setTime = (property, value) => {
+    const temp = {
+      ...formSettings,
+    }
+    temp.times[property] = value
+    setFormSettings(temp)
+  }
+  return (
+    <div>
+      <button>Settings</button>
+      <form onSubmit={saveSettings}>
+        <label>
+          Work:
+          <input
+            type="text"
+            value={formSettings.times.work}
+            onChange={e => setTime("work", e.target.value)}
+          />
+        </label>
+        <label>
+          Short Break:
+          <input
+            type="text"
+            value={formSettings.times.shortBreak}
+            onChange={e => setTime("shortBreak", e.target.value)}
+          />
+        </label>
+        <label>
+          Long Break:
+          <input
+            type="text"
+            value={formSettings.times.longBreak}
+            onChange={e => setTime("longBreak", e.target.value)}
+          />
+        </label>
+        <button type="submit">Save</button>
+      </form>
     </div>
   )
 }
